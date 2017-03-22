2f3f9a172085da071c950b27f2e4a318c25fd667
493296748@qq.com <493296748@qq.com>
2017-03-22 17:05:52 +0800

    添加网络请求

---
+++ app/src/main/java/com/wjf/holographichua/api/ApiConstants.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016 咖枯 <kaku201313@163.com | 3772304@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.wjf.holographichua.api;
+
+public class ApiConstants {
+    public static final String NETEAST_HOST = "http://c.m.163.com/";
+
+    // 头条TYPE
+    public static final String HEADLINE_TYPE = "headline";
+    // 房产TYPE
+    public static final String HOUSE_TYPE = "house";
+    // 其他TYPE
+    public static final String OTHER_TYPE = "list";
+
+    // 头条id
+    public static final String HEADLINE_ID = "T1348647909107";
+    // 房产id
+    public static final String HOUSE_ID = "5YyX5Lqs";
+
+    /**
+     * 新浪图片新闻
+     * http://gank.io/api/data/福利/{size}/{page}
+     */
+    public static final String SINA_PHOTO_HOST = "http://gank.io/api/";
+
+
+    /**
+     * 获取对应的host
+     *
+     * @param hostType host类型
+     * @return host
+     */
+    public static String getHost(int hostType) {
+        String host;
+        switch (hostType) {
+            case HostType.NETEASE_NEWS_VIDEO:
+                host = NETEAST_HOST;
+                break;
+            case HostType.GANK_GIRL_PHOTO:
+                host = SINA_PHOTO_HOST;
+                break;
+            case HostType.NEWS_DETAIL_HTML_PHOTO:
+                host = "http://kaku.com/";
+                break;
+            default:
+                host = "";
+                break;
+        }
+        return host;
+    }
+}
